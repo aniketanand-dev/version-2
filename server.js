351c789a9bdb8786ed9d82280cf9ef685c428c8d
Aniket72094087100 <aniket72094087100@gmail.com>
2026-03-30 02:17:05 +0530

    Version 2 (fix)

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 
 app.get("/", (req, res) => {
   // res.send("Version 1");
-  res.send(undefinedVariable);
+  // res.send(undefinedVariable);
+  res.send("Version 2 fixed");
 });
 
 app.listen(3000, () => console.log("Server running"));
